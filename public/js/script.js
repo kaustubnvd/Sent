570c1fc110d1b31ccab6974b8dfd2b383abe6869
Kaustub Navalady <kaustub.nvd@gmail.com>
2020-08-03 14:13:45 -0500

    Add search results

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,10 @@
 const fromAddress = document.getElementById('from-address');
 const toAddress = document.getElementById('to-address');
 
+// This gets called as callback by the Google Places API
 function autoComplete() {
+  // eslint-disable-next-line no-undef
   new google.maps.places.Autocomplete(fromAddress);
+  // eslint-disable-next-line no-undef
   new google.maps.places.Autocomplete(toAddress);
 }
