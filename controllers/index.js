a200a98488d2a3c9d59bc7c10b8654304857a1dc
Kaustub Navalady <kaustub.nvd@gmail.com>
2020-08-04 17:07:40 -0500

    Rename index

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 exports.getHomePage = (req, res, next) => {
-  res.render('index', {
+  res.render('home', {
     pageTitle: 'Sent | Connecting Texas',
     apiKey: process.env.PLACES_AUTOCOMPLETE_KEY,
   });
