37590e91adbc078d7a06d7b1afd175621bb8daa2
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2020-08-09 18:49:15 -0500

    Add dashboard states

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,7 @@
 exports.getOrderPage = (req, res, next) => {
   res.render('order', {
     carrierName: 'Suket Shah',
-    carrierDesc: 'Can deliver anything that fits in a trunk',
+    carrierDesc: 'Anything that fits in a trunk',
     carrierPrice: '$21',
     fromCity: 'Austin, TX',
     toCity: 'Flower Mound, TX',
