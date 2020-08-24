61c09f40a077885f02d2a98467022de0d38bb188
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2020-08-24 13:07:19 -0500

    :construction:Styling fixes

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -1,18 +1,18 @@
 const User = require('../models/User');
-const fs = require('fs/promises');
+// const fs = require('fs/promises');
 const path = require('path');
 const { validationResult } = require('express-validator');
 
 exports.getSignUpPage = (req, res, next) => {
-  res.render('sign-up', {errors: []});
+  res.render('sign-up', { errors: [] });
 };
 
 exports.postSignUpPage = async (req, res, next) => {
   // Get the form data from the POST request body
-  const { firstName, lastName, email, phoneNumber, password} = req.body;
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    await fs.unlink(path.join('.', req.file.path));
+    // await fs.unlink(path.join('.', req.file.path));
     return res.render('sign-up', {
       errors: errors.array(),
     });
@@ -22,8 +22,15 @@ exports.postSignUpPage = async (req, res, next) => {
   if (req.file) {
     imageURL = '/' + req.file.path;
   }
-  const user = new User({firstName, lastName, email, phoneNum: phoneNumber, password, imageURL});
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    phoneNum: phoneNumber,
+    password,
+    imageURL,
+  });
   await user.save(); // Saves in database
   req.session.user = user;
-  res.redirect('/'); 
+  res.redirect('/');
 };
